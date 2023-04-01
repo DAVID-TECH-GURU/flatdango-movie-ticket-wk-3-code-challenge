4e1269caeb131018109e28a46844b342f88bfcfa
DAVID-TECH-GURU <davidmaina823123@gmail.com>
2023-04-01 16:55:27 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,51 @@
+Flatdango
+
+Flatdango is a web application that allows users to purchase movie tickets from the Flatiron Movie Theater. This project is part of the week 3 code challenge for implementing a 'mini' web application using JavaScript.
+
+Features
+See the first movie's details, including its poster, title, runtime, showtime, and available tickets when the page loads. The number of available tickets is derived by subtracting the number of tickets_sold from the theater's capacity. A GET request is made to the endpoint /films/1 to retrieve the film data.
+See a menu of all movies on the left side of the page in the ul#films element when the page loads. A GET request is made to the endpoint /films to retrieve the film data.
+Buy a ticket for a movie. After clicking the "Buy Ticket" button, the number of available tickets decreases on the frontend. The user cannot buy a ticket if the showing is sold out (if there are 0 tickets available).
+
+Bonus Features
+Click on a movie in the menu to replace the currently displayed movie's details with the new movie's details. An additional GET request is made to retrieve the movie's details.
+When a movie is sold out, indicate that the movie is sold out by changing the button text to "Sold Out". The film item in the ul#films menu is updated by adding a class of sold-out to the film.
+Getting Started
+Clone the repository:
+
+bash
+Copy code
+git clone https://github.com/{DAVID-TECH-GURU}/flatdango.git
+Install the dependencies:
+
+Copy code
+npm install
+Start the development server:
+
+sql
+Copy code
+npm start
+Open the application in your browser:
+
+arduino
+Copy code
+http://localhost:3000
+
+Technologies Used
+JavaScript
+HTML
+CSS
+JSON DB Server
+
+Credits
+This project was created by [DAVID WACHIRA MAINA]. Special thanks to the instructors and TAs at Flatiron School for their support and guidance.
+
+MIT License
+
+Copyright (c) [2023] [DAVID WACHIRA MAINA]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ app.js
@@ -0,0 +1,193 @@
+const filmsUrl = 'http://localhost:3000/films'
+const filmUrl = 'http://localhost:3000/films/1'
+
+const posterEl = document.querySelector('#poster')
+const titleEl = document.querySelector('#title')
+const runtimeEl = document.querySelector('#runtime')
+const showtimeEl = document.querySelector('#showtime')
+const availableTicketsEl = document.querySelector('#available-tickets')
+const buyTicketBtn = document.querySelector('#buy-ticket')
+const filmsListEl = document.querySelector('#films')
+
+//To Load the details of the first movie on page load
+fetch(filmUrl)
+  .then(res => res.json())
+  .then(data => {
+    posterEl.src = data.poster
+    titleEl.innerText = data.title
+    runtimeEl.innerText = `Runtime: ${data.runtime} minutes`
+    showtimeEl.innerText = `Showtime: ${data.showtime}`
+    const availableTickets = data.capacity - data.tickets_sold
+    availableTicketsEl.innerText = `Available Tickets: ${availableTickets}`
+    if (availableTickets === 0) {
+      buyTicketBtn.disabled = true
+    }
+  })
+
+// To Load the list of all movies on page load
+fetch(filmsUrl)
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(film => {
+      const li = document.createElement('li')
+      li.innerText = film.title
+      filmsListEl.appendChild(li)
+    })
+  })
+
+// To Handle the "Buy Ticket" button click
+buyTicketBtn.addEventListener('click', () => {
+  const availableTickets = parseInt(availableTicketsEl.innerText.split(' ')[2])
+  if (availableTickets > 0) {
+    availableTicketsEl.innerText = `Available Tickets: ${availableTickets - 1}`
+  }
+})
+
+function renderMovies(movies) {
+    const movieList = document.querySelector('#films');
+    movieList.innerHTML = '';
+  
+    movies.forEach(movie => {
+      const li = document.createElement('li');
+      li.classList.add('film', 'item');
+      li.id = `movie-${movie.id}`;
+      li.textContent = movie.title;
+  
+      li.addEventListener('click', () => {
+        fetch(`/films/${movie.id}`)
+          .then(response => response.json())
+          .then(movieDetails => {
+            renderMovieDetails(movieDetails);
+          })
+          .catch(error => console.error(error));
+      });
+  
+      movieList.appendChild(li);
+    });
+  }
+
+  
+  function renderMovieDetails(movie) {
+    const movieDetails = document.querySelector('#film-details');
+    movieDetails.innerHTML = '';
+  
+    const title = document.createElement('h2');
+    title.textContent = movie.title;
+  
+    const poster = document.createElement('img');
+    poster.src = movie.poster;
+  
+    const runtime = document.createElement('p');
+    runtime.textContent = `Runtime: ${movie.runtime} minutes`;
+  
+    const description = document.createElement('p');
+    description.textContent = movie.description;
+  
+    const showtime = document.createElement('p');
+    showtime.textContent = `Showtime: ${movie.showtime}`;
+  
+    const capacity = document.createElement('p');
+    capacity.textContent = `Capacity: ${movie.capacity}`;
+  
+    const ticketsSold = document.createElement('p');
+    ticketsSold.textContent = `Tickets sold: ${movie.tickets_sold}`;
+  
+    const buyButton = document.createElement('button');
+    if (movie.tickets_sold >= movie.capacity) {
+      buyButton.textContent = 'Sold Out';
+      buyButton.disabled = true;
+    } else {
+      buyButton.textContent = 'Buy Ticket';
+    }
+  
+    buyButton.addEventListener('click', () => {
+      buyTicket(movie.id);
+    });
+  
+    movieDetails.appendChild(title);
+    movieDetails.appendChild(poster);
+    movieDetails.appendChild(runtime);
+    movieDetails.appendChild(description);
+    movieDetails.appendChild(showtime);
+    movieDetails.appendChild(capacity);
+    movieDetails.appendChild(ticketsSold);
+    movieDetails.appendChild(buyButton);
+  }
+
+  
+  const filmItems = document.querySelectorAll('ul#films li.film.item');
+  filmItems.forEach((filmItem) => {
+    filmItem.addEventListener('click', async (event) => {
+      const id = event.target.dataset.id;
+      const response = await fetch(`http://localhost:3000/films/${id}`);
+      const film = await response.json();
+      const movieDetails = document.querySelector('#movie-details');
+      movieDetails.innerHTML = `
+        <img src="${film.poster}" alt="${film.title} Poster" />
+        <div class="details">
+          <h2>${film.title}</h2>
+          <p>${film.description}</p>
+          <div class="info">
+            <p>Runtime: ${film.runtime} min</p>
+            <p>Showtime: ${film.showtime}</p>
+            <p>Tickets Available: ${film.capacity - film.tickets_sold} / ${film.capacity}</p>
+            <button class="btn buy-button" data-id="${film.id}">${film.capacity - film.tickets_sold > 0 ? 'Buy Ticket' : 'Sold Out'}</button>
+          </div>
+        </div>
+      `;
+    });
+  });
+
+  
+  async function buyTicket(id) {
+    const film = await getFilm(id);
+    if (film.tickets_sold < film.capacity) {
+      const updatedFilm = {
+        ...film,
+        tickets_sold: film.tickets_sold + 1,
+      };
+      await fetch(`http://localhost:3000/films/${id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          tickets_sold: updatedFilm.tickets_sold,
+        }),
+      });
+      displayFilm(updatedFilm);
+    }
+  }
+
+  
+  function displayFilmList(films) {
+    const filmList = document.querySelector('#films');
+    filmList.innerHTML = '';
+    films.forEach(film => {
+      const filmItem = document.createElement('li');
+      filmItem.classList.add('film');
+      const deleteButton = document.createElement('button');
+      deleteButton.innerText = 'Delete';
+      deleteButton.addEventListener('click', async () => {
+        await fetch(`http://localhost:3000/films/${film.id}`, {
+          method: 'DELETE',
+        });
+        filmItem.remove();
+      });
+      const availableTickets = film.capacity - film.tickets_sold;
+      const soldOut = availableTickets === 0 ? 'sold-out' : '';
+      filmItem.innerHTML = `
+        <div class="film-info">
+          <h2 class="film-title">${film.title}</h2>
+          <p class="film-runtime">${film.runtime} min</p>
+          <p class="film-showtime">${film.showtime}</p>
+          <p class="film-description">${film.description}</p>
+          <p class="film-tickets ${soldOut}">${availableTickets} tickets left</p>
+        </div>
+      `;
+      filmItem.appendChild(deleteButton);
+      filmList.appendChild(filmItem);
+    });
+  }
+
+  
